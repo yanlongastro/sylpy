2a713b58b6689aa894c8437db5946a9d09959366
Yanlong Shi <yanlong.astro@gmail.com>
2024-11-28 17:59:08 -0500

    minor update on cluster mass profiles

--- cluster_tools.py
+++ cluster_tools.py
@@ -13,10 +13,28 @@ class cluster:
         self.half_mass_radius_3d = half_mass_radius(mass, position, projection='3d')
         self.half_mass_radius_2d = half_mass_radius(mass, position, projection='2d')
 
+    def mass_profile(self, dN=100, dr=None, dlogr=None, cdf=False, projection='3d'):
+        return spherical_density_profile(self.mass, self.position, dN=dN, dr=dr, dlogr=dlogr, cdf=cdf, projection=projection)
 
-def spherical_density_profile(mass, position, dN=100, cdf=False, projection='3d'):
+
+def spherical_density_profile(mass, position, dN=100, dr=None, dlogr=None, cdf=False, projection='3d'):
     """
-    A function that return the mass distribution
+    For a given mass dist, return the mass profile
+
+    Parameters
+    ----------
+    mass : array
+    position : array
+    dN : int
+        Number of particles to include in each bin, if it's a PDF
+    dr : float
+        The mininmum width of each bin, which avoids noise
+    dlogr : float
+        The mininmum width of each bin in log scales
+    cdf : bool
+        Whether to show the cumulative distribution
+    projection : string, '2d' or '3d'
+        Projection that determines if this is a 3d or 2d distribution function
     """
     i = 0
     if position.ndim>1:
@@ -34,9 +52,17 @@ def spherical_density_profile(mass, position, dN=100, cdf=False, projection='3d'
 
     r = []
     rho = []
-    while i+dN<len(mass):
-        dM = np.sum(mass[i:i+dN])
-        r1 = radius[i+dN] 
+    step = dN
+    while i+step<len(mass):
+        step = dN
+        if dr is not None: # make sure the curve is not too noisy in some regions
+            while radius[i+step]-radius[i]<dr:
+                step += 1
+        if dlogr is not None:
+            while np.log10(radius[i+step]/radius[i])<dlogr:
+                step += 1
+        dM = np.sum(mass[i:i+step])
+        r1 = radius[i+step] 
         r0 = radius[i]
         if projection=='3d':
             dV = (r1**3-r0**3)*np.pi*4/3
@@ -46,7 +72,7 @@ def spherical_density_profile(mass, position, dN=100, cdf=False, projection='3d'
         r_tmp = (r0+r1)/2
         r.append(r_tmp)
         rho.append(rho_tmp)
-        i += dN
+        i += step
     return np.array(r), np.array(rho)
 
 def half_mass_radius(mass, position, projection='3d'):
