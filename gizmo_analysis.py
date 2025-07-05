efc2a7138daa7a2482cb88b71ede86a3f22032d2
Yanlong Shi <yanlong.astro@gmail.com>
2025-07-04 23:06:04 -0400

    minor updates

--- gizmo_analysis.py
+++ gizmo_analysis.py
@@ -66,9 +66,9 @@ def parse_path(path, skip=0):
 # Unit: Msun, pc, yr
 def set_star_softening(M, Res):
     dM = M/Res**3
-    drho = 2227.359223512722/Na
-    drho /= Msun_in_g /pc_in_cm**3
-    dr = (dM/drho)**(1/3)
+    rho = 1000/Na # 1000 mp/cm^3
+    rho /= Msun_in_g /pc_in_cm**3
+    dr = (3/4/np.pi *dM/rho)**(1/3) /2.8
     # print(dM, drho)
     return dr
 
